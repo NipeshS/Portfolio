bf79d8fde4594ee37fab9d070d2b5a32cc931abc
NipeshS <nipeshnaresh62@gmail.com>
2026-04-29 10:20:52 +0530

    Update About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,7 @@ function About() {
                   <strong>Process Associate</strong> - Tata Consultancy Services (Aug 2023 - Dec 2024)
                 </p>
                 <p className="mb-0">
-                  <strong>Pursuing Java Full Stack Development</strong> - Internz Learn (Jan 2025 - Present)
+                  <strong>Pursuing Java Developer</strong> - Stackly (Mar 2026 - Present)
                 </p>
               </div>
             </div>
